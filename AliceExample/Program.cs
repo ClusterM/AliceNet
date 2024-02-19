acf093e06c6864c7c7327353fbaa556e1c50ec6a
Alexey Cluster <cluster@cluster.wtf>
2024-02-19 12:24:37 +0400

    Example :)

--- AliceExample/Program.cs
+++ AliceExample/Program.cs
@@ -1,40 +1,63 @@
 ﻿using wtf.cluster.AliceNet;
 using wtf.cluster.AliceNet.Types;
+using wtf.cluster.AliceNet.Types.Request.Entities;
+using wtf.cluster.AliceNet.Types.Request.RequestBody;
 using wtf.cluster.AliceNet.Types.Response;
 
 namespace AliceExample
 {
     internal class Program
     {
-        static async void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var server = new AliceServer("http://localhost:8080");
+            var server = new AliceServer("http://localhost:8080/alice/");
             server.Start();
             await Task.Delay(Timeout.InfiniteTimeSpan);
         }
     }
 
-    internal class AliceServer(string localEndpoint) : AliceServerBase(localEndpoint)
+    internal class AliceServer(string localEndpoint) : AliceServerBase(localEndpoint, logger: null)
     {
-        protected override Task<AliceResponse> HandleRequest(AliceReqest reqest,
-            CancellationToken cancellationToken = default)
+        protected override async Task<AliceResponse> HandleRequest(AliceReqest request, CancellationToken cancellationToken = default)
         {
-            return Task.FromResult(
-                new AliceResponse
+            if (request.Session.New)
+            {
+                return new AliceResponse
                 {
                     Response = new ResponseBody
                     {
-                        Text = "Привет, это Алиса!",
+                        Text = "Привет, это Алиса! Скажи триста!",
                         Buttons = [
-                            new Button
-                            {
-                                Title = "Открыть репозиторий",
-                                Url = "https://github.com/ClusterM/AliceNet"
-                            }
-                        ]
+                                new Button
+                                {
+                                    Title = "Открыть репозиторий",
+                                    Url = "https://github.com/ClusterM/AliceNet"
+                                }
+                            ]
                     }
-                }
-            );
+                };
+            }
+            else if (request.RequestBody is SimpleUtterance su && su.Nlu.Entities.Any(e => e is NumberEntity n && n.Value == 300))
+            {
+                return new AliceResponse
+                {
+                    Response = new ResponseBody
+                    {
+                        Text = "Отсоси у тракториста!",
+                        Tts = "xxx <[schwa t s schwa  ss ii]> у тракториста! <speaker audio=\"alice-sounds-game-win-1.opus\">"
+                    }
+                };
+            }
+            else
+            {
+                return new AliceResponse
+                {
+                    Response = new ResponseBody
+                    {
+                        Text = "Ну скажи триста..."
+                    }
+                };
+            }
         }
     }
 }
